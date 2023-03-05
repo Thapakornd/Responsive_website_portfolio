1395f4d8f821a871404625e1c6ea6cf8443d363a
Galez <gustheworld6@gmail.com>
2023-03-06 00:29:09 +0700

    v0.1
    Complete

---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+let menu = document.querySelector("#menu-click");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+    navbar.classList.toggle("active");
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
+
+const sr = ScrollReveal({
+    distance: '60px',
+    duration: 2500,
+    reset: true
+})
+
+sr.reveal('.home-text',{delay:150, origin:'top'})
+sr.reveal('.home-img',{delay:400, origin:'top'})
+
+sr.reveal('.about, .services, .work, .contact-content, .copyright', {delay:150, origin:'top'})
